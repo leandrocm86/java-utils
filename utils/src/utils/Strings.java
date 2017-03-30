1a9ebc8c60893c20d33eecc33191fb00602d101f
leandrocm86 <leandrocm86@31ab2af0-2d96-4b6a-b000-07bc552cd8b1>
2017-03-30 12:05:42 +0000

    Acrescentando mais estatisticas

--- utils/src/utils/Strings.java
+++ utils/src/utils/Strings.java
@@ -2,8 +2,13 @@ package utils;
 
 import java.io.PrintWriter;
 import java.io.StringWriter;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class Strings {
+	
+	private static SimpleDateFormat TIME_FORMAT;
 	
 	public static boolean empty(String s) {
 		return s == null || s.equals("");
@@ -14,5 +19,15 @@ public class Strings {
 		PrintWriter pw = new PrintWriter(sw);
 		t.printStackTrace(pw);
 		return sw.toString(); // stack trace as a string
+	}
+	
+	public static Date parseTime(String time) throws ParseException {
+		if (TIME_FORMAT == null)
+			TIME_FORMAT = new SimpleDateFormat("hh:mm:ss,SSS");
+		if (time.length() == 5)
+			time += ":00,000";
+		else if (time.length() == 8)
+			time += ",000";
+		return TIME_FORMAT.parse(time);
 	}
 }
