b05cf491d098c2320fab4fa7e439df1e0b24efe6
06742776609 <leandro.medeiros@serpro.gov.br>
2020-07-08 11:58:42 -0300

    Ajuste

--- utils/src/utils/Data.java
+++ utils/src/utils/Data.java
@@ -4,7 +4,7 @@ import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
-public class Data extends Date {
+public class Data extends Date implements Objeto {
 	
 	private static final long serialVersionUID = 1L;
 	
