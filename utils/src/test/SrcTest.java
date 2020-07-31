37cea80c5d9b011cadfa06da1bd71695ded07f60
06742776609 <leandro.medeiros@serpro.gov.br>
2020-07-31 17:41:59 -0300

    Retirando testes unitários

--- utils/src/test/SrcTest.java
+++ utils/src/test/SrcTest.java
@@ -1,11 +1,5 @@
 package test;
-import static org.junit.Assert.assertEquals;
-
-import org.junit.Test;
-
 import estruturas.Lista;
-import utils.Data;
-import utils.Str;
 
 public class SrcTest {
 
@@ -47,27 +41,27 @@ public class SrcTest {
 		
 	}
 	
-	@Test
-	public void testeSubstrings() {
-		Str frutas = new Str("abacaxi, banana, maçã, abacaxi, abacate, uva, abacaxi, morango, laranja");
-		confere("abacaxi, abacate, uva, abacaxi, morango, laranja", frutas.desdeEnesimo("abacaxi", 2));
-		confere("abacaxi, abacate, uva, ", frutas.desdeEnesimo("abacaxi", 2).ateEnesimo("abacaxi", 2));
-		confere("abacate, uva, abacaxi, morango, laranja", frutas.desdeEnesimo(" ", 4, false));
-		confere("abacaxi, banana, maçã, abacaxi", frutas.ateEnesimo("abacaxi", 2, true));
-	}
-	
-	@Test
-	public void testeDatas() {
-		Data data1 = new Data("2020-07-31 09:24:09,767", "yyyy-MM-dd HH:mm:ss,SSS");
-		Data data2 = new Data("12:30:00", Data.HORA_HH_mm_ss);
-		Data data3 = new Data(Long.parseLong("1596216131918"));
-		assertEquals(9, data1.getHours());
-		assertEquals(12, data2.getHours());
-		assertEquals(14, data3.getHours());
-	}
-	
-	private void confere(String esperado, Str resultado) {
-		assertEquals(new Str(esperado), resultado);
-	}
+//	@Test
+//	public void testeSubstrings() {
+//		Str frutas = new Str("abacaxi, banana, maçã, abacaxi, abacate, uva, abacaxi, morango, laranja");
+//		confere("abacaxi, abacate, uva, abacaxi, morango, laranja", frutas.desdeEnesimo("abacaxi", 2));
+//		confere("abacaxi, abacate, uva, ", frutas.desdeEnesimo("abacaxi", 2).ateEnesimo("abacaxi", 2));
+//		confere("abacate, uva, abacaxi, morango, laranja", frutas.desdeEnesimo(" ", 4, false));
+//		confere("abacaxi, banana, maçã, abacaxi", frutas.ateEnesimo("abacaxi", 2, true));
+//	}
+//	
+//	@Test
+//	public void testeDatas() {
+//		Data data1 = new Data("2020-07-31 09:24:09,767", "yyyy-MM-dd HH:mm:ss,SSS");
+//		Data data2 = new Data("12:30:00", Data.HORA_HH_mm_ss);
+//		Data data3 = new Data(Long.parseLong("1596216131918"));
+//		assertEquals(9, data1.getHours());
+//		assertEquals(12, data2.getHours());
+//		assertEquals(14, data3.getHours());
+//	}
+//	
+//	private void confere(String esperado, Str resultado) {
+//		assertEquals(new Str(esperado), resultado);
+//	}
 
 }
