2536e94390dc014180c7375c2546e745ba8f45bb
Leandro Medeiros <leandro.medeiros@serpro.gov.br>
2021-05-20 11:23:58 -0300

    Aprimoramento do Benchmark para não gerar erros.

--- utils/src/utils/Benchmark.java
+++ utils/src/utils/Benchmark.java
@@ -11,24 +11,42 @@ import java.util.Map.Entry;
 public class Benchmark {
 	private static HashMap<String, Long> temposAcumulados = new LinkedHashMap<>();
 	private static HashMap<String, Long> corridasEmAndamento = new HashMap<String, Long>();
+	private static HashMap<String, Integer> corridasNaoEncontradas = new HashMap<String, Integer>();
 	
 	public static void start(String corrida) {
 		corridasEmAndamento.put(corrida, System.currentTimeMillis());
 	}
 	
 	public static void stop(String corrida) {
-		long duracao = System.currentTimeMillis() - corridasEmAndamento.get(corrida);
-		Long tempoAcumulado = temposAcumulados.get(corrida);
-		if (tempoAcumulado == null)
-			tempoAcumulado = 0L;
-		tempoAcumulado += duracao;
-		temposAcumulados.put(corrida, tempoAcumulado);
+		Long inicio = corridasEmAndamento.get(corrida);
+		if (inicio == null) {
+			registraCorridaNaoEncontrada(corrida);
+		} else {
+			long duracao = System.currentTimeMillis() - inicio;
+			Long tempoAcumulado = temposAcumulados.get(corrida);
+			if (tempoAcumulado == null)
+				tempoAcumulado = 0L;
+			tempoAcumulado += duracao;
+			temposAcumulados.put(corrida, tempoAcumulado);
+		}
 	}
 	
+	private static void registraCorridaNaoEncontrada(String corrida) {
+		Integer vezesNaoEncontrada = corridasNaoEncontradas.get(corrida);
+		if (vezesNaoEncontrada == null)
+			vezesNaoEncontrada = 0;
+		corridasNaoEncontradas.put(corrida, ++vezesNaoEncontrada);
+	}
+
 	public static String getResultado() {
 		String resultado = "";
 		for (Entry<String, Long> entry : temposAcumulados.entrySet())
 			resultado += entry.getKey() + ": " + Str.formataTempo(entry.getValue()) + "\n";
+		if (!corridasNaoEncontradas.isEmpty()) {
+			resultado += "\n!!! OBSERVAÇÃO: HOUVE PEDIDOS PARA PARAR CORRIDAS QUE NÃO ESTAVAM CORRENDO:\n";
+			for (Entry<String, Integer> entry : corridasNaoEncontradas.entrySet())
+				resultado += new Str("'--' (-- vezes)\n", entry.getKey(), entry.getValue());
+		}
 		return resultado;
 	}
 
